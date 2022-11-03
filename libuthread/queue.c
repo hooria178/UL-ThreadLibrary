53393b2701c2e3d019a7e4725952777d998fdd16
hooria178 <72269201+hooria178@users.noreply.github.com>
2022-11-03 03:22:23 -0700

    Update queue.c

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 #include "queue.h"
 
@@ -20,25 +21,29 @@ struct queue {
 queue_t queue_create(void)
 {
 	/* TODO Phase 1 */
-	return (queue_t)malloc(sizeof(struct queue));
-
+	queue_t q = (queue_t)malloc(sizeof(struct queue));
+  q->first = NULL;
+  q->last = NULL;
+  q->length = 0;
+  return q;
 }
 
 int queue_destroy(queue_t queue)
 {
 	/* TODO Phase 1 */
-	if(queue)
+	/* If queue->first == NULL or queue == NULL, then queue is empty*/
+	if(!queue || queue->length != 0)
 	{
-		free(queue);
-		return 0;
+		return -1;
 	}
-	return -1;
-	
+  	free(queue);
+	return 0;
 }
 
 int queue_enqueue(queue_t queue, void *data)
 {
 	/* TODO Phase 1 */
+  /*
 	if((queue == NULL) || (data == NULL))
 	{
 		return -1;
@@ -51,23 +56,46 @@ int queue_enqueue(queue_t queue, void *data)
 	}
 	else // if queue is not empty, then add the data item at the back aka end of the queue
 	{
-		queue->last->data = *data;
+		queue->last->data = data;
 		queue->last = queue->last->after;
 	}
-	queue->size++;
+	queue->length++;
 	return 0;
+  */
+
+  struct listItem* newItem = (struct listItem*)malloc(sizeof(struct listItem));
+  if(!queue || !data || !newItem)
+	{
+		return -1;
+	}
+  newItem->data = data;
+  newItem->before = queue->last;
+  newItem->after = NULL;
+  if(queue->last)
+  {
+	queue->last->after = newItem;
+  }
+  queue->last = newItem;
+  if(!queue->first){
+    queue->first = newItem;
+  }
+  queue->length++;
+  return 0;
 }
 
 int queue_dequeue(queue_t queue, void **data)
 {
 	/* TODO Phase 1 */
-	if(!queue)
+	if(!queue || !data || queue->length == 0)
 	{
 		return -1;
 	}
 	*data = queue->first->data;
 	queue->first = queue->first->after;
-	return 0; 
+	/* NEED TO FREE queue->first?*/
+	//free(queue->first);
+  	queue->length--;
+	return 0;
 }
 
 int queue_delete(queue_t queue, void *data)
@@ -78,16 +106,34 @@ int queue_delete(queue_t queue, void *data)
 		return -1;
 	}
 	struct listItem *current = queue->first;
-	while(current->data != *data)
+	while(current->data != data)
 	{
+		//printf("COMPARING %d to %d, (%p, %p)", *(int*)current->data, *(int*)data, current->data, data);
 		if(!current->after)
 		{
 			return -1;
 		}
-		*current = current->after;
+		current = current->after;
 	}
-	current->before->after = current->after;
-	current->after->before = current->before;
+
+	if(current->before)
+	{
+		current->before->after = current->after;
+	}
+	else
+	{
+		queue->first = current->after;
+	}
+
+	if(current->after)
+	{
+		current->after->before = current->before;
+	}
+	else
+	{
+		queue->last = current->before;
+	}
+  	queue->length--;
 	return 0;
 }
 
@@ -98,8 +144,15 @@ int queue_iterate(queue_t queue, queue_func_t func)
 	{
 		return -1;
 	}
+  struct listItem* current = queue->first;
+  while(current)
+  {
+    void* funcdata = current->data;
+    func(queue, funcdata);
+    current = current->after;
+  }
 	return 0;
-	
+
 }
 
 int queue_length(queue_t queue)
@@ -112,3 +165,15 @@ int queue_length(queue_t queue)
 	return queue->length;
 }
 
+void queue_print(queue_t queue)
+{
+  printf("Queue[%d]:\n", queue->length);
+  struct listItem *current = queue->first;
+  int i = 0;
+  while(current)
+  {
+    printf("[%d]: %d\n", i, *(int*)current->data);
+    i++;
+    current = current->after;
+  }
+}
