339430d3358934b5fec18ef017b3085a86aa1572
hooria178 <72269201+hooria178@users.noreply.github.com>
2022-11-03 23:20:47 -0700

    Update sem.c

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -4,13 +4,14 @@
 #include "queue.h"
 #include "sem.h"
 #include "private.h"
+#include  "uthread.h"
 
 /* semaphore struct containing semCount and semQueue as properties */
 struct semaphore
 {
     size_t semCount;
     queue_t semQueue;
-    //sem->lock ?
+
 };
 
 /* sem_create() allocates memory to the new semaphore,
@@ -18,7 +19,7 @@ struct semaphore
 */
 sem_t sem_create(size_t count)
 {
-    sem_t sem = (sem_t)malloc(sizeof(sem_t)); // multiply by count?
+    sem_t sem = (sem_t)malloc(sizeof(sem_t));
     sem->semCount = count;
     sem->semQueue = queue_create();
 
@@ -30,9 +31,7 @@ sem_t sem_create(size_t count)
 */
 int sem_destroy(sem_t sem)
 {
-    /* Error: if @sem is NULL or there are other threads still being
-        blocked on @sem
-    */
+
     if (sem == NULL || (sem->semQueue != NULL))
     {
         return -1;
@@ -41,67 +40,38 @@ int sem_destroy(sem_t sem)
     queue_destroy(sem->semQueue);
     free(sem);
 
-    // if (!free(sem))
-    // {
-    //     return -1;
-    // }
-
     return 0;
-    /* Error management: return -1 if @sem is NULL
-    or if other threads are still being blocked on @sem;
-    return 0 if sem was successfully destroyed*/
 }
 
 
 int sem_down(sem_t sem)
 {
-    /* TODO Phase 3 */
     if (sem == NULL)
     {
         return -1;
     }
-    spinlock_lock(sem->lock);
-    while ((sem->count) == 0)
-    {
-        /*Block self            */
-
-        /* ...                  */
-        /* Call uthread_block() */
-        uthread_t currentThread = uthread_current();
-        queue_enqueue(sem->semQueue, (void*)currentThread);
+    if(sem->semCount == 0){
+        queue_enqueue(sem->semQueue, uthread_current());
         uthread_block();
     }
-    sem->count -= 1;
-    spinlock_unlock(sem->lock);
+    else if(sem->semCount > 0){
+        sem->semCount--;
+    }
     return 0;
-    /* Error management: return -1 if @sem is NULL; return 0 if sem was successfully taken*/
 }
 
 int sem_up(sem_t sem)
 {
-    /* TODO Phase 3 */
     if (sem == NULL)
     {
         return -1;
     }
-    spinlock_lock(sem->lock);
-    sem->count += 1;
-    /* Wake up first in line */
-    /* (if any)             */
-    /* ...                  */
-    /* Call uthread_unblock() */
-    if(sem->semQueue != NULL)
-    {
-        uthread_t currentThread;
+    if(sem->semCount == 0){
+        struct uthread_tcb* currentThread;
         queue_dequeue(sem->semQueue, (void**)&currentThread);
         uthread_unblock(currentThread);
     }
-    spinlock_unlock(sem->lock);
+    sem->semCount++;
+
     return 0;
-    /* Error management: return -1 if @sem is NULL; return 0 if sem was successfully released*/
 }
-/* go to the lectures recordings regarding synchronization
- and specifically focus on the count part and the block part of slide 20 and 21.*/
-
-
-/* NEED TO FIGURE OUT THE SPINLOCK DEAL AND SO BASICALLY SEM_UP and SEM_DOWN functions*/
